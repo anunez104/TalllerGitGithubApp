038e57abdfacf3dcb088cf9f40dea6bbeebf95b6
angie nuñez <anunez104@unab.edu.co>
2026-02-20 17:36:01 -0500

    feat: agrega imagen personal

--- app/src/main/java/me/angienunez/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/angienunez/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.angienunez.tallergitgithubapp.ui.theme.TallerGitGithubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +31,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TallerGitGithubAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TrabajoEnClase()
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TrabajoEnClase() {
+    Column(
+        modifier = Modifier.padding(all = 90.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val painter = painterResource(R.drawable.maxito)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TallerGitGithubAppTheme {
-        Greeting("Android")
+        Image(
+            painter = painter,
+            contentDescription = "f1",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(width = 200.dp)
+                .height(200.dp)
+                .clip(CircleShape)
+        )
     }
 }
